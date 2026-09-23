3fd566612e016c3fda59fb5da087775d5e23c8b0
Emmanuel <efernandezg@truper.com>
2026-09-23 15:36:19 -0600

    Cambie BrowserRouter por Hashrouter (a fin de que al actualizar no se caiga la pagina)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import  ListaComponentes from './Componentes/ListaComponentes'
-import HeaderComponent from './Componentes/HeaderComponent'
-import { HashRouter as BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaComponentes from './Componentes/ListaComponentes';
+import HeaderComponent from './Componentes/HeaderComponent';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './Componentes/Inicio';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginForm from './Componentes/LoginForm';
 import AgregarUsuario from './Componentes/AgregarUsuario';
-import MatrizCD from './Componentes/MatrizCD'
+import MatrizCD from './Componentes/MatrizCD';
 import NuevaPO from './Componentes/NuevaPO';
 import HistorialContenedor from './Componentes/materialReutilizable/historialContenedor';
 import Administrador_documentos from './Componentes/Vistas_Onboarding/Administrador_documentos';
@@ -27,212 +27,189 @@ import Matriz_Planta from './Componentes/Planta_Matr_Soc/Matriz_Planta';
 import Menu_Planta_Planta from './Componentes/Planta_Matr_Soc/Menu_Planta_Planta';
 
 function App() {
-  
-  const almacenlocalusuario = localStorage.getItem('username')
-  const almacenlocalpassword = localStorage.getItem('password')
-  const [user,setUser] = useState({username:"",password:""});
-  const[error,setError] =useState("");
+  const almacenlocalusuario = localStorage.getItem('username');
+  const almacenlocalpassword = localStorage.getItem('password');
+  const [user, setUser] = useState({ username: '', password: '' });
+  const [error, setError] = useState('');
 
-const Login = async usuarioinfo =>{
-    if ( usuarioinfo.perfil === "admin" ||  usuarioinfo.perfil === "usuarioinicial"  ||  
-         usuarioinfo.perfil === "usuarioseguimiento" || usuarioinfo.perfil ==="ControlDocumental" || 
-         usuarioinfo.perfil ==="Documentos" || usuarioinfo.perfil === "SeguimientoOC1"  || 
-         usuarioinfo.perfil === "Matr/Soc" ||  usuarioinfo.perfil === "ControlPlanta" 
-         ||  usuarioinfo.perfil === "CalculadoraPlanta"  ) {
+  const Login = async (usuarioinfo) => {
+    if (
+      usuarioinfo.perfil === 'admin' ||
+      usuarioinfo.perfil === 'usuarioinicial' ||
+      usuarioinfo.perfil === 'usuarioseguimiento' ||
+      usuarioinfo.perfil === 'ControlDocumental' ||
+      usuarioinfo.perfil === 'Documentos' ||
+      usuarioinfo.perfil === 'SeguimientoOC1' ||
+      usuarioinfo.perfil === 'Matr/Soc' ||
+      usuarioinfo.perfil === 'ControlPlanta' ||
+      usuarioinfo.perfil === 'CalculadoraPlanta'
+    ) {
       setUser({
-        username:usuarioinfo.usuario,
-        password:usuarioinfo.constrasena})
-        localStorage.setItem('username', usuarioinfo.usuario)
-        localStorage.setItem('perfil', usuarioinfo.perfil)
-        
-    }
-    else if (usuarioinfo.perfil === "formatos"){
-  setUser({
-        username:usuarioinfo.usuario,
-        password:usuarioinfo.constrasena})
-        localStorage.setItem('username', usuarioinfo.usuario)
-        localStorage.setItem('perfil', usuarioinfo.perfil)
-    }
-    else{
-      if(usuarioinfo === "NuevoUser") {
+        username: usuarioinfo.usuario,
+        password: usuarioinfo.constrasena,
+      });
+      localStorage.setItem('username', usuarioinfo.usuario);
+      localStorage.setItem('perfil', usuarioinfo.perfil);
+    } else if (usuarioinfo.perfil === 'formatos') {
+      setUser({
+        username: usuarioinfo.usuario,
+        password: usuarioinfo.constrasena,
+      });
+      localStorage.setItem('username', usuarioinfo.usuario);
+      localStorage.setItem('perfil', usuarioinfo.perfil);
+    } else {
+      if (usuarioinfo === 'NuevoUser') {
         setUser({
-          username:usuarioinfo.usuario,
-          password:usuarioinfo.constrasena})
-          localStorage.setItem('username', usuarioinfo)
-      }else{
-      setError("Usuario / Contraseña incorrectos")
+          username: usuarioinfo.usuario,
+          password: usuarioinfo.constrasena,
+        });
+        localStorage.setItem('username', usuarioinfo);
+      } else {
+        setError('Usuario / Contraseña incorrectos');
+      }
+    }
+  };
+
+  if (almacenlocalusuario === null) {
+    return (
+      <div>
+        <HashRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/record" element={<LoginForm Login={Login} error={error} />} />
+              <Route path="*" element={<LoginForm Login={Login} error={error} />} />
+            </Routes>
+          </div>
+        </HashRouter>
+      </div>
+    );
+  } else {
+    if (almacenlocalusuario === 'NuevoUser') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record/usuario" element={<AgregarUsuario />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'ControlDocumental') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<Inicio />} />
+                <Route path="/importaciones/controldocumental/matrizcd" element={<MatrizCD />} />
+                <Route path="/importaciones/controldocumental/matrizcd/NuevaPO" element={<NuevaPO />} />
+                <Route path="/importaciones/controldocumental/matrizcd/historialCD" element={<HistorialContenedor />} />
+                <Route path="/importaciones/controldocumental/matrizcd/calculadora" element={<CalculadoraC />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'Documentos') {
+      return (
+        <div style={{ backgroundColor: '#f2f2f2' }}>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<Inicio />} />
+                <Route path="/importaciones/AdmonDocs" element={<Administrador_documentos />} />
+                <Route path="/importaciones/Sesiones" element={<Sesiones />} />
+                <Route path="/importaciones/inscritos" element={<Inscritos />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'SeguimientoOC1') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<Socs />} />
+                <Route path="/importaciones/controldocumental/matrizcd/log-detalle" element={<SocsLog />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'formatos') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<MenuFormatos />} />
+                <Route path="/record/formatotrial" element={<FormatoTrial />} />
+                <Route path="/record/formatorevisados" element={<FormatoRevisados />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'Matr/Soc') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<MenuSocMatriz />} />
+                <Route path="/record/Soc" element={<Socs />} />
+                <Route path="/importaciones/controldocumental/matrizcd/log-detalle" element={<SocsLog />} />
+                <Route path="/record/Inicio" element={<Inicio />} />
+                <Route path="/record/importaciones/controldocumental/matrizcd" element={<MatrizCD />} />
+                <Route path="/importaciones/controldocumental/matrizcd/NuevaPO" element={<NuevaPO />} />
+                <Route path="/importaciones/controldocumental/matrizcd/historialCD" element={<HistorialContenedor />} />
+                <Route path="/importaciones/controldocumental/matrizcd/calculadora" element={<CalculadoraC />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'ControlPlanta') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<Menu_Matriz_Soc_Planta />} />
+                <Route path="/record/planta/soc_planta" element={<Soc_Planta />} />
+                <Route path="/record/planta/matriz_planta" element={<Matriz_Planta />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
+    } else if (localStorage.getItem('perfil') === 'CalculadoraPlanta') {
+      return (
+        <div>
+          <HashRouter>
+            <HeaderComponent />
+            <div className="container">
+              <Routes>
+                <Route path="/record" element={<Menu_Planta_Planta />} />
+                <Route path="/record/Calculadoraplanta" element={<CalculadoraC />} />
+              </Routes>
+            </div>
+          </HashRouter>
+        </div>
+      );
     }
   }
-  }
-
-if(almacenlocalusuario === null){
-  return (
-    <div>
-         <BrowserRouter>
-      <div className='container'>
-      <Routes>
-        <Route  path='record/' element={<LoginForm Login={Login} error={error} />}></Route>    
-      </Routes>
-      </div>
-      </BrowserRouter>
-   
-  </div>
-)
-}else{
-  if(almacenlocalusuario ==="NuevoUser"){
-
-    return(
-      <div>
-      <BrowserRouter>
-      <HeaderComponent />
-      <div className='container'>
-      <Routes>
-        <Route  path='record/usuario' element={<AgregarUsuario/>}></Route>    
-      </Routes>
-      </div>
-      </BrowserRouter>
-    </div>
-    )    
-  }
- else  if(localStorage.getItem('perfil') === "ControlDocumental"){
-    return(
-      <div>
-      <BrowserRouter>
-      <HeaderComponent />
-      <div className='container'>
-      <Routes>
-         <Route exact path='record/' element={<Inicio/>}></Route>
-          <Route path='importaciones/controldocumental/matrizcd' element={<MatrizCD/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/NuevaPO' element={<NuevaPO/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/historialCD' element={<HistorialContenedor/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/calculadora' element={<CalculadoraC/>}></Route>
-      </Routes>
-      </div>
-      </BrowserRouter>
-    </div>
-    )
-  }else if(localStorage.getItem('perfil') === "Documentos"){
-    return(
-      <div style={{ backgroundColor: '#f2f2f2'}}>
-      <BrowserRouter>
-      <HeaderComponent />
-      <div className='container'>
-      <Routes>
-            <Route exact path='record/' element={<Inicio/>}></Route>
-           <Route  path='importaciones/AdmonDocs' element={<Administrador_documentos/>}></Route>   
-           <Route  path='importaciones/Sesiones' element={<Sesiones/>}></Route>   
-           <Route  path='importaciones/inscritos' element={<Inscritos/>}></Route>   
-      </Routes>
-      </div>
-      </BrowserRouter>
-    </div>
-    )
-  } 
-  else if(localStorage.getItem("perfil") === "SeguimientoOC1"){
-    return (
-      <div>
-         <BrowserRouter>
-      <HeaderComponent/>
-        <div className='container'>
-        <Routes>
-          <Route  path='record/' element={<Socs/>}></Route>
-          <Route path='importaciones/controldocumental/matrizcd/log-detalle' element={<SocsLog/>}></Route>
-        </Routes>
-      </div>
-      </BrowserRouter>
-      </div>
-    )
-  }
-  else if (localStorage.getItem("perfil") === "formatos") {
-    return (
-      <div>
-         <BrowserRouter>
-      <HeaderComponent/>
-        <div className='container'>
-        <Routes>
-          <Route  path='record/' element={<MenuFormatos/>}></Route>
-          <Route  path='record/formatotrial' element={<FormatoTrial/>}></Route>
-          <Route  path='record/formatorevisados' element={<FormatoRevisados/>}></Route>
-        </Routes>
-      </div>
-      </BrowserRouter>
-      </div>
-    )
-  }
-    else if (localStorage.getItem("perfil") === "Matr/Soc") {
-    return (
-      <div>
-         <BrowserRouter>
-      <HeaderComponent/>
-        <div className='container'>
-        <Routes>
-          <Route  path='record/' element={<MenuSocMatriz/>}></Route>
-          <Route  path='record/Soc' element={<Socs/>}></Route>
-          <Route path='importaciones/controldocumental/matrizcd/log-detalle' element={<SocsLog/>}></Route>
-          <Route path='record/Inicio' element={<Inicio/>}></Route>
-          <Route path='record/importaciones/controldocumental/matrizcd' element={<MatrizCD/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/NuevaPO' element={<NuevaPO/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/historialCD' element={<HistorialContenedor/>}></Route>    
-          <Route path='importaciones/controldocumental/matrizcd/calculadora' element={<CalculadoraC/>}></Route>
-        </Routes>
-      </div>
-      </BrowserRouter>
-      </div>
-    )
-
-  }    else if (localStorage.getItem("perfil") === "ControlPlanta") {
-    return (
-      <div>
-         <BrowserRouter>
-      <HeaderComponent/>
-        <div className='container'>
-        <Routes>
-          <Route  path='record/' element={<Menu_Matriz_Soc_Planta/>}></Route>
-          <Route  path='record/planta/soc_planta' element={<Soc_Planta/>}></Route>
-          <Route  path='record/planta/matriz_planta' element={<Matriz_Planta/>}></Route>
-        </Routes>
-      </div>
-      </BrowserRouter>
-      </div>
-    )
-
-  }
-
-      else if (localStorage.getItem("perfil") === "CalculadoraPlanta") {
-    return (
-      <div>
-         <BrowserRouter>
-      <HeaderComponent/>
-        <div className='container'>
-        <Routes>
-          <Route  path='record/' element={<Menu_Planta_Planta/>}></Route>
-          <Route  path='record/Calculadoraplanta' element={<CalculadoraC/>}></Route>
-        </Routes>
-      </div>
-      </BrowserRouter>
-      </div>
-    )
-
-  }
-
-//   if ( localStorage.getItem("perfil") === "admin" ||  localStorage.getItem("perfil") === "usuarioinicial"  ||  localStorage.getItem("perfil") === "usuarioseguimiento") {
-//   return(
-//   <div>
-//   <BrowserRouter>
-//   <HeaderComponent/>
-//   <div className='container'>
-//   <Routes>
-//     <Route exact path='record/' element={<Inicio/>}></Route>
-//     <Route  path='record/clientes' element={<ListaComponentes/>}></Route>
-//     <Route  path='record/add-Clientes' element={<AddClientesComponent/>}></Route>
-//     <Route  path='record/edit-Clientes/:id' element={<AddClientesComponent/>}></Route>
-//     <Route  path='record/usuario' element={<AgregarUsuario/>}></Route>
-
-//   </Routes>
-//   </div>
-//   </BrowserRouter>
-// </div>
-// )}
-};}
+}
 
 export default App;
